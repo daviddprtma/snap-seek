435d0023940169f6e5f1ad791942e59359e4cd28
daviddprtma <davidchristianpratama@gmail.com>
2024-11-06 10:03:18 +0700

    first commit

---
+++ constant/locales.ts
@@ -0,0 +1,15 @@
+export default {
+  'ar-SA': 'العربية',
+  'de-DE': 'Deutsch',
+  'en-US': 'English',
+  'es-ES': 'Español',
+  'fr-FR': 'Français',
+  'ja-JP': '日本語',
+  'ko-KR': '한국어',
+  'pt-BR': 'Português',
+  'ru-RU': 'Русский',
+  'zh-CN': '简体中文',
+  'zh-TW': '繁體中文',
+  'bn-BD': 'বাংলা', // Bengali (Bangladesh)
+  'hi-IN': 'हिन्दी', // Hindi (India)
+} as const;
